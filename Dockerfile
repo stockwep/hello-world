622881b54e845e0d7b42ba2f04acdd2ae9d64d12
Steve Lasker <stevenlasker@hotmail.com>
2020-10-23 15:52:14 -0700

    Initial commit
    Signed-off-by: Steve Lasker <stevenlasker@hotmail.com>

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+#FROM node:9-alpine
+FROM demo42t.azurecr.io/base-artifacts/node:9-alpine
+EXPOSE 80
+COPY . /src
+RUN cd /src && npm install
+CMD ["node", "/src/server.js"]
---
+++ server.js
@@ -0,0 +1,27 @@
+var http = require('http')
+var os = require("os");
+
+var port = 80
+
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+ 
+  response.write('<style type="text/css">body {background-color:'+process.env.BACKGROUND_COLOR+';}</style>');
+ 
+  response.write('<h1>Azure Container Registry Tasks</h1>');
+  response.write('<h3>Enabling OS & Framework Patching</h3>');
+  response.write('<a href="https://aka.ms/acr/tasks">https://aka.ms/acr/tasks</a></p>');
+  response.write('<a href="https://aka.ms/teleport/signup">https://aka.ms/teleport/signup</a></p>');
+
+  response.write('<p>Hello Orlando-afternoon-I"m committed</p>');
+
+  response.write('<p><b>Version:</b> '+process.env.NODE_VERSION+'</p>');
+  response.write('<p><b>Background Color:</b> '+process.env.BACKGROUND_COLOR+'</p>');
+  response.write('<p><b>HostName:</b> '+os.hostname()+'</p>');
+  
+  response.end();
+})
+
+server.listen(port)
+
+console.log('Server running at http://localhost:' + port)
