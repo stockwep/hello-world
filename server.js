5cafe29d16472c2f6d2a519455e2b252705bb0ea
Steve Lasker <stevenlasker@hotmail.com>
2020-10-23 18:42:01 -0700

    Change server.js for new build
    Signed-off-by: Steve Lasker <stevenlasker@hotmail.com>

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ var server = http.createServer(function (request, response) {
   response.write('<h1>Azure Container Registry Tasks</h1>');
   response.write('<h3>Enabling OS & Framework Patching -3</h3>');
   response.write('<a href="https://aka.ms/acr/tasks">https://aka.ms/acr/tasks</a></p>');
-  response.write('<a href="https://aka.ms/teleport/signup">https://aka.ms/teleport/signup</a></p>');
 
   response.write('<p><b>Version:</b> '+process.env.NODE_VERSION+'</p>');
   response.write('<p><b>Background Color:</b> '+process.env.BACKGROUND_COLOR+'</p>');
