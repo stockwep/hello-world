acf7ecace8e15532fae871b9fac1e2209c6a69fb
Philip Stockwell <stockwep@outlook.com>
2021-08-04 12:05:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var server = http.createServer(function (request, response) {
   response.write('<a href="https://aka.ms/acr/tasks">https://aka.ms/acr/tasks</a></p>');
 
   response.write('<p><b>Hello World</b></p>');
+  response.write('<p><b>Updated</b></p>');
   response.write('<p><b>Version:</b> '+process.env.NODE_VERSION+'</p>');
   response.write('<p><b>Background Color:</b> '+process.env.BACKGROUND_COLOR+'</p>');
   response.write('<p><b>HostName:</b> '+os.hostname()+'</p>');
